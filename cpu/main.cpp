472f991b4fa3cd2ae9fd1e4806db6786bd758014
Denis Dedkov <dephyys@gmail.com>
2021-10-30 15:55:57 +0300

    Create processor client application

--- cpu/main.cpp
+++ cpu/main.cpp
@@ -3,26 +3,34 @@
 #include <unistd.h>
 
 #include <iomap.h>
-
 #include "executor.h"
 
 int main(int argc, char *argv[])
 {
-        printf("Hello from cpu!!!\n");
+        thrw(exit(1), (argc != 2), "Invalid arguments count\n");
+
+        int error = 0;
 
         cpu_t cpu = {0};
-        construct_cpu(&cpu);
-
-        size_t size = 0;
         char *buf = nullptr;
 
-        map_in(argv[1], &buf, &size);
+        size_t size = 0;
 
-        execute(&cpu, buf);
+        error = construct_cpu(&cpu);
+        thrw(goto cup1, (error), "Can't create cpu\n");
 
+        error = map_in(argv[1], &buf, &size);
+        thrw(goto cup1, (error), "Can't map binary file\n");
+
+        error = execute(&cpu, buf);
+        thrw(goto cup2, (error), "Error during execution\n");
+
+cup2:
         munmap(buf, size);
-
+cup1:
         destruct_cpu(&cpu);
 
         return 0;
 }
+
+
